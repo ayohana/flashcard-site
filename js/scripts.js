0057fd94bc4b2758931b78529c04822e59bb6a29
Adela <adela.yohana@gmail.com>
2020-01-14 15:12:35 -0800

    Add more cards, total of 5

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,16 @@ $(document).ready(function() {
   $("#card1").click(function() {
     $("#card1 .cardContent").slideToggle();
   });
-});
-
-$(document).ready(function() {
   $("#card2").click(function() {
     $("#card2 .cardContent").slideToggle();
+  });
+  $("#card3").click(function() {
+    $("#card3 .cardContent").slideToggle();
+  });
+  $("#card4").click(function() {
+    $("#card4 .cardContent").slideToggle();
+  });
+  $("#card5").click(function() {
+    $("#card5 .cardContent").slideToggle();
   });
 });
